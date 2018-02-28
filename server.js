139667b9592f449f587f17f0b9aac9ac2eccb12a
kashif zahid <kashif.zahid18@gmail.com>
2018-02-28 16:42:12 +0000

    removing app.get

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const PORT = process.env.port || 8080;
-app.get('/', function (req, res) {
+/*app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
     
-})
+})*/
 io.on('connection', function (socket) {
     console.log("one user connected" + socket.id);
     
