f3812be379f4e9becb2a347042a518aceacb09d9
kashif zahid <kashif.zahid18@gmail.com>
2018-03-03 13:46:23 +0000

    PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const PORT = process.env.port || 8080;
+const PORT = process.env.PORT || 8080;
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
     
