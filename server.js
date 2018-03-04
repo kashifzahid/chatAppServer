4b3e5be3970246cb35521b692c85be0162ebe937
kashif zahid <kashif.zahid18@gmail.com>
2018-03-04 10:20:48 +0000

    user added

--- server.js
+++ server.js
@@ -3,15 +3,19 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const PORT = process.env.PORT || 8080;
+var connectedUsers = {};
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
     
 })
 io.on('connection', function (socket) {
     console.log("one user connected" + socket.id);
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
     
     socket.on('message', function (data) {
-        console.log(data);
+        console.log(data.user+":"+data.message);
         io.emit("message", { message: data });
         
     })
