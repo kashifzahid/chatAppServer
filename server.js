5ed1537dd44d8376257f63c629b75284afaeaad2
kashif zahid <kashif.zahid18@gmail.com>
2018-03-08 11:10:37 +0000

    first"

--- server.js
+++ server.js
@@ -3,20 +3,27 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const PORT = process.env.PORT || 8080;
-var connectedUsers = {};
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
     
 })
 io.on('connection', function (socket) {
     console.log("one user connected" + socket.id);
+    socket.on('join', function (data) {
+        socket.join(data.user);
+        // We are using room of socket io
+    });
     socket.on('disconnect', function () {
         console.log('user disconnected');
     });
     
     socket.on('message', function (data) {
-        console.log(data.user+":"+data.message);
-        io.emit("message", { message: data });
+        console.log(data.message);
+        io.sockets.in(data.user).emit("new_msg", { message: data });
+        console.log("message sent to" + data.user);
+        
+       // io.emit("message", { message: data });
         
     })
     
